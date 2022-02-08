ee6e5a507117b4080ffb3946f53e8eb06fdc8e35
LeonardoComar <leofcomar@gmail.com>
2022-02-08 18:02:19 -0300

    Correções

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,22 @@ class Product < ApplicationRecord
 
   belongs_to :unit
 
+  before_create :set_status, :set_quantity
+
+  validates :name, :price, presence: true
+  validates :price, numericality: true
+
   def available?
     self.status == 'available'
+  end
+
+  private
+  
+  def set_status
+    self.status = "unavailable"
+  end
+  
+  def set_quantity
+    self.quantity = 0
   end
 end
